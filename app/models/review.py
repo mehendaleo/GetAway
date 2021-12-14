de6a294034fd60e5392dfa04177ca27272b8346c
mehendaleo <mehendaleomkar@gmail.com>
2021-12-14 18:57:06 -0500

    added all models, need relationships

---
+++ app/models/review.py
@@ -0,0 +1,16 @@
+from .db import db
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+    def to_dict(self):
+        return {
+            'user_id': self.user_id,
+            'location_id': self.location_id,
+            'content': self.content
+        }
