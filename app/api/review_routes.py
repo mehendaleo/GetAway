b59d66218b30301f3a3895cabc906a639766e782
mehendaleo <mehendaleomkar@gmail.com>
2021-12-29 12:50:48 -0500

    added full CRUD for reviews

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, request
 from flask_login import login_required
-from app.models import Review, db
+from app.models import Review, db, review
 from app.forms import CreateReview, UpdateReview
 
 review_routes = Blueprint('reviews', __name__)
 
 # all reviews for specific location
-@review_routes.route('/location/<int:location_id>')
+@review_routes.route('/<int:location_id>')
 # @login_required
 def reviews(location_id):
     query_reviews = Review.query.filter(Review.location_id == location_id).all()
@@ -14,7 +14,7 @@ def reviews(location_id):
     return {'reviews': reviews}
 
 # create a review
-@review_routes.route('/location/create', methods=['POST'])
+@review_routes.route('/create', methods=['POST'])
 # @login_required
 def create_review():
     form = CreateReview()
@@ -45,5 +45,18 @@ def update_review(review_id):
 
         db.session.commit()
         return review.to_dict()
+    else:
+        return 'bad data'
+
+
+# delete a review
+@review_routes.route('/<int:review_id>/delete', methods=['DELETE'])
+# @login_required
+def delete_review(review_id):
+    review = Review.query.get(review_id)
+    if review:
+        db.session.delete(review)
+        db.session.commit()
+        return 'Successfully Deleted'
     else:
         return 'bad data'
